934988ec5de5542a9a2197cd24734ea552ddb565
Hectarea <hectometrocuadrado@gmail.com>
2023-11-26 14:47:56 +0100

    Restored the special treatment of declarations.
    The special treatment makes declarations work as one 'should expect'.

--- clith.lisp
+++ clith.lisp
@@ -13,6 +13,7 @@
   (:documentation
    "Destructs an object bound by a LET*-like or MULTIPLE-VALUE-BIND-like form at the end of a WITH macro call.")
   (:method (obj)
+    (declare (ignore obj))
     (values)))
 
 
@@ -46,6 +47,94 @@
     (loop for binding in bindings
 	  do (check-binding binding)))
 
+  (defun var-declaration-p (id)
+    (member id '(dynamic-extent ignore ignorable special type)))
+
+  (defun func-declaration-p (id)
+    (member id '(dynamic-extent ignore ignorable inline not-inline ftype)))
+
+  (defun var-func-declaration-p (id)
+    (member id '(dynamic-extent ignore ignorable)))
+  
+  (defun extract-declaration-single-var (var declarations)
+    (if declarations
+        (let* ((current-declaration (car declarations))
+               (rest-declarations (cdr declarations))
+               (id (car current-declaration))
+               (vars (if (eq id 'type)
+                         (cddr current-declaration)
+                         (cdr current-declaration)))
+               (var-type (when (eq id 'type)
+                           (list (cadr current-declaration)))))
+          (multiple-value-bind (extracted-declarations body-declarations)
+              (extract-declaration-single-var var rest-declarations)
+            (if (and (var-declaration-p (car current-declaration))
+                     (member var vars))
+                (let* ((new-extracted-declarations (cons `(,id ,@var-type ,var)
+                                                         extracted-declarations))
+                       (new-body-declarations (if (= (length vars) 1)
+                                                  body-declarations
+                                                  (cons `(,id ,@var-type ,@(remove var vars))
+                                                        body-declarations))))
+                  (values new-extracted-declarations
+                          new-body-declarations))
+                (values extracted-declarations
+                        (cons current-declaration body-declarations)))))
+        (values nil nil)))
+
+  (defun extract-declaration-single-func (func declarations)
+    (if declarations
+        (let* ((current-declaration (car declarations))
+               (rest-declarations (cdr declarations))
+               (id (car current-declaration))
+               (complete-func (if (var-func-declaration-p id)
+                                  `(function ,func)
+                                  func))
+               (funcs (if (eq id 'ftype)
+                          (cddr current-declaration)
+                          (cdr current-declaration)))
+               (func-type (when (eq id 'ftype)
+                            (list (cadr current-declaration)))))
+          (multiple-value-bind (extracted-declarations body-declarations)
+              (extract-declaration-single-func func rest-declarations)
+            (if (and (func-declaration-p (car current-declaration))
+                     (member complete-func funcs :test #'equal))
+                (let* ((new-extracted-declarations (cons `(,id ,@func-type ,complete-func)
+                                                         extracted-declarations))
+                       (new-body-declarations (if (= (length funcs) 1)
+                                                  body-declarations
+                                                  (cons `(,id ,@func-type ,@(remove complete-func funcs :test #'equal))
+                                                        body-declarations))))
+                  (values new-extracted-declarations
+                          new-body-declarations))
+                (values extracted-declarations
+                        (cons current-declaration body-declarations)))))
+        (values nil nil)))
+
+  (defun extract-var-declarations (vars declarations)
+    (if vars
+        (let ((var (car vars))
+              (rest-vars (cdr vars)))
+          (multiple-value-bind (extracted-declarations rest-declarations)
+              (extract-var-declarations rest-vars declarations)
+            (multiple-value-bind (new-extracted-declarations new-rest-declarations)
+                (extract-declaration-single-var var rest-declarations)
+              (values (append extracted-declarations new-extracted-declarations)
+                      new-rest-declarations))))
+        (values nil declarations)))
+
+  (defun extract-func-declarations (funcs declarations)
+    (if funcs
+        (let ((func (car funcs))
+              (rest-funcs (cdr funcs)))
+          (multiple-value-bind (extracted-declarations rest-declarations)
+              (extract-func-declarations rest-funcs declarations)
+            (multiple-value-bind (new-extracted-declarations new-rest-declarations)
+                (extract-declaration-single-func func rest-declarations)
+              (values (append extracted-declarations new-extracted-declarations)
+                      new-rest-declarations))))
+        (values nil declarations)))
+
   (defun let-binding-p (binding)
     (or (symbolp binding)
         (and (listp binding)
@@ -64,10 +153,44 @@
   (defun nest-expression-p (binding)
     (and (listp binding)
          (= (length binding) 1)))
+  
+  (defun extract-binding-declarations (bindings declarations)
+    (if bindings
+        (let ((binding (car bindings))
+              (rest-bindings (cdr bindings)))
+          (multiple-value-bind (extracted-declarations rest-declarations)
+              (extract-binding-declarations rest-bindings declarations)
+            (cond
+              ((or (let-binding-p binding)
+                   (multiple-value-binding-p binding))
+               (let ((syms (cond
+                             ((symbolp binding)
+                              (list binding))
+                             ((symbolp (car binding))
+                              (list (car binding)))
+                             (t
+                              (car binding)))))
+                 (multiple-value-bind (new-extracted-declarations new-rest-declarations)
+                     (extract-var-declarations syms rest-declarations)
+                   (values (cons new-extracted-declarations extracted-declarations)
+                           new-rest-declarations))))
+              ((label-binding-p binding)
+               (let ((funcs (list (car binding))))
+                 (multiple-value-bind (new-extracted-declarations new-rest-declarations)
+                     (extract-func-declarations funcs rest-declarations)
+                   (values (cons new-extracted-declarations extracted-declarations)
+                           new-rest-declarations))))
+              ((nest-expression-p binding)
+               (values (cons nil extracted-declarations)
+                       rest-declarations)))))
+        (values nil declarations)))
 
   (defun collect-let-bindings (bindings)
     (loop for (binding . rest-bindings) on bindings
-          while (let-binding-p binding)
+          while (or (symbolp binding)
+                    (and (listp binding)
+                         (= (length binding) 2)
+                         (symbolp (car binding))))
           for last-rest-bindings = rest-bindings
           collect binding into let-bindings
           finally (return (values let-bindings last-rest-bindings))))
@@ -75,27 +198,33 @@
   (defun collect-multiple-value-binding (bindings)
     (let ((binding (car bindings))
           (rest-bindings (cdr bindings)))
-      (when (multiple-value-binding-p binding)
+      (when (and (listp binding)
+                 (= (length binding) 2)
+                 (listp (car binding)))
         (values binding rest-bindings))))
 
   (defun collect-labels-bindings (bindings)
     (loop for (binding . rest-bindings) on bindings
-          while (label-binding-p binding)
+          while (and (listp binding)
+                     (> (length binding) 2))
           for last-rest-bindings = rest-bindings
           collect binding into labels-bindings
           finally (return (values labels-bindings last-rest-bindings))))
 
   (defun collect-nest-expressions (bindings)
     (loop for (binding . rest-bindings) on bindings
-          while (nest-expression-p binding)
+          while (and (listp binding)
+                     (= (length binding) 1))
           for last-rest-bindings = rest-bindings
           collect binding into nest-expressions
           finally (return (values nest-expressions last-rest-bindings))))
   
-  (defun make-let-form (bindings body)
+  (defun make-let-form (bindings body declaration)
     (let* ((proper-bindings (remove-if #'symbolp bindings))
            (vars-to-destroy (mapcar #'car proper-bindings)))
       `(let* ,bindings
+         ,@(when declaration
+             `((declare ,@declaration)))
          ,(if vars-to-destroy
               `(unwind-protect
                     ,body
@@ -105,60 +234,66 @@
                              (reverse vars-to-destroy))))
               body))))
 
-  (defun make-multiple-value-bind-form (binding body)
+  (defun make-multiple-value-bind-form (binding body declaration)
     `(multiple-value-bind ,@binding
+         ,@(when declaration
+             `((declare ,@declaration)))
        (unwind-protect
             ,body
          (destroyer ,(caar binding)))))
 
-  (defun make-labels-form (bindings body)
+  (defun make-labels-form (bindings body declaration)
     `(labels ,bindings
+       ,@(when declaration
+           `((declare ,@declaration)))
        ,body))
 
   (defun make-nest-form (bindings body)
     (let ((nest-expressions (mapcar #'car bindings)))
       `(uiop:nest ,@nest-expressions ,body)))
 
-  (defun make-with-form (bindings body)
-    (cond
-      (bindings
-       (multiple-value-bind (let-bindings rest-bindings) (collect-let-bindings bindings)
-         (when let-bindings
-           (return-from make-with-form
-             (make-let-form let-bindings (make-with-form rest-bindings body)))))
-       (multiple-value-bind (multi-binding rest-bindings) (collect-multiple-value-binding bindings)
-         (when multi-binding
-           (return-from make-with-form
-             (make-multiple-value-bind-form multi-binding (make-with-form rest-bindings body)))))
-       (multiple-value-bind (labels-bindings rest-bindings) (collect-labels-bindings bindings)
-         (when labels-bindings
-           (return-from make-with-form
-             (make-labels-form labels-bindings (make-with-form rest-bindings body)))))
-       (multiple-value-bind (nest-exprs rest-bindings) (collect-nest-expressions bindings)
-         (when nest-exprs
-           (return-from make-with-form
-             (make-nest-form nest-exprs (make-with-form rest-bindings body))))))
-      (t
-       `(locally
-            ,@body)))))
+  (defun make-with-form (bindings body binding-declarations body-declarations)
+    (let ((binding-declaration (car binding-declarations))
+          (rest-binding-declarations (cdr binding-declarations)))
+      (cond
+        (bindings
+         (multiple-value-bind (let-bindings rest-bindings) (collect-let-bindings bindings)
+           (when let-bindings
+             (return-from make-with-form
+               (make-let-form let-bindings (make-with-form rest-bindings body rest-binding-declarations
+                                                           body-declarations)
+                              binding-declaration))))
+         (multiple-value-bind (multi-binding rest-bindings) (collect-multiple-value-binding bindings)
+           (when multi-binding
+             (return-from make-with-form
+               (make-multiple-value-bind-form multi-binding (make-with-form rest-bindings body
+                                                                               rest-binding-declarations
+                                                                               body-declarations)
+                                            binding-declaration))))
+         (multiple-value-bind (labels-bindings rest-bindings) (collect-labels-bindings bindings)
+           (when labels-bindings
+             (return-from make-with-form
+               (make-labels-form labels-bindings (make-with-form rest-bindings body rest-binding-declarations
+                                                                 body-declarations)
+                                 binding-declaration))))
+         (multiple-value-bind (nest-exprs rest-bindings) (collect-nest-expressions bindings)
+           (when nest-exprs
+             (return-from make-with-form
+               (make-nest-form nest-exprs (make-with-form rest-bindings body rest-binding-declarations
+                                                          body-declarations))))))
+        (t
+         (if body-declarations
+             `(locally
+                  (declare ,@body-declarations)
+                ,@body)
+             `(progn
+                ,@body)))))))
 
-
-;; Each binding can be:
-;; A symbol: Then it works like (let* (symbol) ...)
-;; A list with one element. It works like (uiop:nest element (progn ...))
-;; A list with two elements, where the second one is a lisp expression:
-;;   - The first element is a symbol. Then it work like (let* ((symbol expr)) ... (with-destructor symbol))
-;;   - The first element is a list of symbols. Then it works like (multiple-value-bind symbols expr ... (with-destructor (car symbols)))
-;; A list with more than two elements. Then it works like (labels (first-elem second-elem rest-elems) ...)
-
-;; Tell the story about LET, MULTIPLE-VALUE-BIND and UIOP:NEST having and adventure and having the child WITH. Later, they adopt the pet |DESTROYER|
-;; When saying they had a pet, show the installation code.
-;; When WITH is saying its first words, show the equivalent LET*, MULTIPLE-VALUE-BIND and UIOP:NEST code but with WITH.
 
 (defmacro with (bindings &body body)
   "This macro has the following systax:
 
-  (WITH (binding*) declaration* expr*)
+  (WITH (binding*) declaration expr*)
 
   binding                  ::= nest-form | let-form | multiple-value-bind-form | labels-form
   nest-form                ::= (expr)
@@ -167,8 +302,8 @@
   labels-form              ::= (func lambda-list expr+) 
   var                      ::= symbol
 
-WITH is a combination of LET*, MULTIPLE-VALUE-BIND, UIOP:NEST and LABELS. It can bind variables, bind functions
- and nest expressions.
+WITH is a combination of LET*, MULTIPLE-VALUE-BIND, UIOP:NEST and LABELS. It can bind variables, functions and
+ nest expressions.
 
 WITH accepts a list of binding clauses. Each binding clause must be a symbol or a list. Depending of what the
 clause is, WITH's behaeviour is different:
@@ -180,9 +315,8 @@ clause is, WITH's behaeviour is different:
       
       --- Expands to ---
       (let* (x)
-        (locally 
-          (setf x 5) 
-          (print x)))
+        (setf x 5)
+        (print x))
 
   - A list with one element: Works like UIOP:NEST.
       
@@ -191,11 +325,9 @@ clause is, WITH's behaeviour is different:
         (print k))
 
       --- Expands to ---
-      (uiop/utility:nest 
-        x 
-        (let ((k 5))) 
-        (locally 
-          (print k)))
+      (UIOP/UTILITY:NEST X 
+                         (LET ((K 5))) 
+                         (PROGN (PRINT K)))
 
   - A list with two elements: Works like LET* or MULTIPLE-VALUE-BIND. Also, the function DESTROYER is called
     with the bound variables at the end of the WITH macro. Using the MULTIPLE-VALUE-BIND form will result in
@@ -210,11 +342,11 @@ clause is, WITH's behaeviour is different:
         (unwind-protect
             (multiple-value-bind (a b c) (values 4 5 6)
               (unwind-protect 
-                  (locally 
+                  (progn 
                     (print x)) 
                 (destroyer a)))
-         (progn 
-           (destroyer x))))
+          (progn 
+            (destroyer x))))
 
   - A list with at least three elements. Works like LABELS.
 
@@ -225,36 +357,45 @@ clause is, WITH's behaeviour is different:
       --- Expands to ---
       (labels ((hello (name)
                  (format t \"hello ~a\" name)))
-        (locally 
+        (progn 
           (hello)))
 
 Artificial example computing fibonacci numbers:
 
-      (with (((a b) (values 0 1))
+      (with (((*a* *b*) (values 0 1))
              (one-step ()
-               (let ((aux (+ a b)))
-                 (setf a b
-                       b aux)))
+               (let ((aux (+ *a* *b*)))
+                 (setf *a* *b*
+                       *b* aux)))
              ((loop for i from 1 to 10 do (one-step))))
-        (declare (optimize (speed 3)))
-        (print a))
+        (declare (special *a* *b*) (optimize (speed 3)))
+        (print *a*))
 
       --- Expands to ---
-      (multiple-value-bind (a b) (values 0 1)
+      (multiple-value-bind (*a* *b*) (values 0 1)
+        (declare (special *b*) (special *a*))
         (unwind-protect
             (labels ((one-step ()
-                       (let ((aux (+ a b)))
-                         (setf a b
-                               b aux))))
-              (uiop/utility:nest
-                (loop for i from 1 to 10
-                      do (one-step))
-                (locally 
-                  (declare (optimize (speed 3))) 
-                  (print a))))
-          (destroyer a)))"
+                       (let ((aux (+ *a* *b*)))
+                         (setf *a* *b*
+                               *b* aux))))
+              (uiop/utility:nest (loop for i from 1 to 10
+                                       do (one-step))
+                                 (locally 
+                                   (declare (optimize (speed 3))) 
+                                   (print *a*))))
+          (destroyer *a*)))"
   (check-bindings bindings)
-  (make-with-form bindings body))
+  (let* ((declarations (when (and body
+                                  (listp (car body))
+                                  (eq (caar body) 'declare))
+                         (cdar body)))
+         (real-body (if declarations
+                        (cdr body)
+                        body)))
+    (multiple-value-bind (extracted-declarations body-declarations)
+        (extract-binding-declarations bindings declarations)
+      (make-with-form bindings real-body extracted-declarations body-declarations))))
 
 
 (defmethod destroyer ((obj stream))
